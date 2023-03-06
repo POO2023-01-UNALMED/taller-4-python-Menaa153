055c51269b781c70cdc141b7a2ec4ded13b14ac1
Menaa153 <cimbacuan@unal.edu.co>
2023-03-05 23:05:12 -0500

    test

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -1,8 +1,11 @@
 class Asignatura:
 
-    def __init__(self, nombre, salon):
+    def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        if self._salon is None:
+            return self._nombre
+        else:
+            return f"{self._nombre} {self._salon}"
